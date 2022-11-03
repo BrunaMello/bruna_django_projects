7fd4dc7581e525188054138698e547ca071025ab
brunamello <brunacrespomello@gmail.com>
2022-11-03 12:30:38 +0000

    Template Config 8

--- bruna_django_projects/settings.py
+++ bruna_django_projects/settings.py
@@ -61,8 +61,7 @@ ROOT_URLCONF = 'bruna_django_projects.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
