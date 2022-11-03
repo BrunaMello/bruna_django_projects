7d3b60bc50a88625d82699a622336d155a50ef64
brunamello <brunacrespomello@gmail.com>
2022-11-03 13:40:29 +0000

    Template Config 12

--- blog/views.py
+++ blog/views.py
@@ -5,17 +5,17 @@ from .models import Post
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'blog/Index.html')
 
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('created_on')
-    template_name = 'BlogPage.html'
+    template_name = 'blog/BlogPage.html'
 
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'BlogPost.html'
+    template_name = 'blog/BlogPost.html'
 
 
 
